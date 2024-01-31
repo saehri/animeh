d39629edeb184200ed14da9e008d60af9602e647
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-01-31 13:28:59 +0700

    feature/server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello world!');
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log('server running on PORT:', PORT);
+});
